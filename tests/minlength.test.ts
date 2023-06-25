3264b1c234296fc2c2b3ae34cb990fe948832dd9
Ivan Akimov <sqids@fossfox.com>
2023-06-25 08:47:19 -0500

    wip

--- tests/minlength.test.ts
+++ tests/minlength.test.ts
@@ -14,6 +14,31 @@ test('simple', () => {
 	expect.soft(sqids.decode(id)).toEqual(numbers);
 });
 
+test('incremental numbers', () => {
+	const sqids = new Sqids({
+		...defaultOptions,
+		minLength: defaultOptions.alphabet.length
+	});
+
+	const ids = {
+		jf26PLNeO5WbJDUV7FmMtlGXps3CoqkHnZ8cYd19yIiTAQuvKSExzhrRghBlwf: [0, 0],
+		vQLUq7zWXC6k9cNOtgJ2ZK8rbxuipBFAS10yTdYeRa3ojHwGnmMV4PDhESI2jL: [0, 1],
+		YhcpVK3COXbifmnZoLuxWgBQwtjsSaDGAdr0ReTHM16yI9vU8JNzlFq5Eu2oPp: [0, 2],
+		OTkn9daFgDZX6LbmfxI83RSKetJu0APihlsrYoz5pvQw7GyWHEUcN2jBqd4kJ9: [0, 3],
+		h2cV5eLNYj1x4ToZpfM90UlgHBOKikQFvnW36AC8zrmuJ7XdRytIGPawqYEbBe: [0, 4],
+		'7Mf0HeUNkpsZOTvmcj836P9EWKaACBubInFJtwXR2DSzgYGhQV5i4lLxoT1qdU': [0, 5],
+		APVSD1ZIY4WGBK75xktMfTev8qsCJw6oyH2j3OnLcXRlhziUmpbuNEar05QCsI: [0, 6],
+		P0LUhnlT76rsWSofOeyRGQZv1cC5qu3dtaJYNEXwk8Vpx92bKiHIz4MgmiDOF7: [0, 7],
+		xAhypZMXYIGCL4uW0te6lsFHaPc3SiD1TBgw5O7bvodzjqUn89JQRfk2Nvm4JI: [0, 8],
+		'94dRPIZ6irlXWvTbKywFuAhBoECQOVMjDJp53s2xeqaSzHY8nc17tmkLGwfGNl': [0, 9]
+	};
+
+	for (const [id, numbers] of Object.entries(ids)) {
+		expect.soft(sqids.encode(numbers)).toBe(id);
+		expect.soft(sqids.decode(id)).toEqual(numbers);
+	}
+});
+
 test('min lengths', () => {
 	const sqids = new Sqids();
 
