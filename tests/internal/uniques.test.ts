b677c94b8622068e75f3c9e08a41af4360c5ddb4
Ivan Akimov <sqids@fossfox.com>
2023-09-10 18:38:18 -0500

    testing uniques with 1m + random

--- tests/internal/uniques.test.ts
+++ tests/internal/uniques.test.ts
@@ -4,7 +4,7 @@ import Sqids, { defaultOptions } from '../../src/index.ts';
 // @NOTE: "uniques, with blocked words" is auto-tested since a lot of these big ids
 // will match some words on the blocklist and will be re-generated anyway
 
-const upTo = 10_000_000;
+const upTo = 1_000_000;
 
 test('uniques', () => {
 	const sqids = new Sqids();
@@ -52,14 +52,14 @@ test('uniques, with padding', () => {
 	expect.soft(numbersSet.size).toBe(upTo);
 });
 
-test('uniques, with multiple numbers', () => {
+test('uniques, with multiple/random numbers', () => {
 	const sqids = new Sqids();
 
 	const idSet = new Set<string>();
 	const numbersSet = new Set<string>();
 
 	for (let i = 0; i != upTo; i++) {
-		const numbers = [0, i, i + 1, 100, 999999999];
+		const numbers = [0, i, i + 1, Math.floor(Math.random() * 1_000), 999999999];
 
 		const id = sqids.encode(numbers);
 
