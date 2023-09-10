20980aa2c4b7e8f4bbc0aa750b29b49326f1f092
Ivan Akimov <sqids@fossfox.com>
2023-09-10 18:55:39 -0500

    testing uniques with 5m + random

--- tests/internal/uniques.test.ts
+++ tests/internal/uniques.test.ts
@@ -4,7 +4,7 @@ import Sqids, { defaultOptions } from '../../src/index.ts';
 // @NOTE: "uniques, with blocked words" is auto-tested since a lot of these big ids
 // will match some words on the blocklist and will be re-generated anyway
 
-const upTo = 2_000_000;
+const upTo = 5_000_000;
 
 test('uniques', () => {
 	const sqids = new Sqids();
